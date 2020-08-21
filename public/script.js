376070c2fa83a3035273ea9212b75e497a7c56ca
Cleber Santos <cleber7284@gmail.com>
2020-08-21 08:30:42 -0300

    start add hide-show

--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
 
 const cards = document.querySelectorAll('.card')
 
+const button = document.querySelector('.btn-toggle')
+
+const content = document.querySelector('.content')
+
 for (let i = 0; cards.length; i++) {
 
     cards[i].addEventListener("click", function(){
         window.location.href = `/recipes/${i}`
     })
-
 }
 
+
+button.addEventListener('click', function(){
+
+})
 
 // event.preventDefault() // evita o refresh da pagína.
