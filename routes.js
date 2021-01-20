199908d70f38201a3fbf374ec39bdb6d2cbfdbd3
cleber-santos <cleber7284@gmail.com>
2021-01-20 09:57:36 -0300

    create admin foodfy

---
+++ routes.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const routes = express.Router()
+
+//HOME
+
+//INDEX
+routes.get('/', function(req, res){
+    return res.render("home/index")
+})
+
+// SOBRE
+routes.get('/sobre', function(req,res){
+    return res.render("home/sobre")
+})
+
+// ADMIN
+
+// MOSTRAR A LISTA DE RECEITAS
+routes.get('/recipes', function(req,res) {
+    return res.render("admin/recipes")
+})
+
+// Mostrar formulário de nova receita
+routes.get('/admin/recipes/create', function (req,res) {
+    return res.render("admin/recipes/create")
+})
+
+// exibir detalhes de uma receita
+routes.get('/admin/show', function(req,res) {
+    return res.render("admin/recipes/show")
+})
+
+
+
+module.exports = routes
+
+// routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+// routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+
+// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+// routes.put("/admin/recipes", recipes.put); // Editar uma receita
+// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
--- server.js
+++ server.js
@@ -1,50 +1,19 @@
-// importar express
 const express = require('express')
-
-// importar nunjucks
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
 
-// criar servidor
 const server = express()
-const recipes = require("./data")
+// const recipes = require("./data")
 
-
-// Configurar o servidor para a pasta public
 server.use(express.static('public'))
+server.use(routes)
 
-// Template engine
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server,
     autoescape: false,
-})
-
-// iniciar rota
-server.get("/", function(req,res){
-    return res.render("index", {items: recipes})
-})
-
-server.get("/sobre", function(req,res){
-    const classBody = {
-        name: "sobre",
-    }
-    return res.render("sobre", { classBody: classBody })
-})
-
-server.get("/receitas", function(req,res){
-    const classBody = {
-        name: "receitas",
-    }
-
-    return res.render("receitas", { classBody: classBody, items: recipes})
-})
-
-server.get("/recipes/:index", function(req,res){
-
-    const recipeIndex = req.params.index
-
-    return res.render("recipes", {item: recipes[recipeIndex]})
+    noCache: true
 })
 
 
